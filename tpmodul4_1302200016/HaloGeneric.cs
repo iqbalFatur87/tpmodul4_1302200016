a23a1b2afc2961876d80d3f5eb315bdfaabdb297
iqbalFatur87 <iqbalfatur18@gmail.com>
2022-03-16 21:55:10 +0700

    menambahkan class HaloGeneric dengan method SapaUser

---
+++ tpmodul4_1302200016/HaloGeneric.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_1302200016
+{
+    internal class HaloGeneric<G>
+    {
+        public void SapaUser(G input)
+        {
+            Console.WriteLine("Hai User" + input); 
+        }
+
+    }
+}
--- tpmodul4_1302200016/Program.cs
+++ tpmodul4_1302200016/Program.cs
@@ -4,13 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Nama : Muhamad Iqbal F
+// NIM : 1302200016
+// Kelas : SE4402
+
 namespace tpmodul4_1302200016
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            HaloGeneric<String> haloGeneric = new HaloGeneric<string>();
 
+            haloGeneric.SapaUser("Iqbal");
         }
     }
 }
